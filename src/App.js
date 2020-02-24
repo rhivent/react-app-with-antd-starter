18f67d683c80231125fce9ae93c8d15b4d00abec
Riventus <riventus@aksaramaya.com>
2020-02-24 11:28:53 +0700

    starter ui for react with ant design + override babel

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { Breadcrumb ,Button,DatePicker,version,message,Alert } from 'antd';
+// import 'antd/dist/antd.css';
+// import { DatePicker } from 'antd';
 import './App.css';
 
 function App() {
+  const [ date,setdate ] = React.useState(null);
+
+  const handleChange = date => {
+    // message.info(`You Selected Date: ${date ? date.format('YYYY-MM-DD') : 'None'}`);
+    setdate(date);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Breadcrumb>
+        <Breadcrumb.Item>Home</Breadcrumb.Item>
+        <Breadcrumb.Item>
+          <a href="#">Application Center</a>
+        </Breadcrumb.Item>
+        <Breadcrumb.Item>
+          <a href="#">Application List</a>
+        </Breadcrumb.Item>
+        <Breadcrumb.Item>An Application</Breadcrumb.Item>
+      </Breadcrumb>
+    <h1>antd version: {version}</h1>
+    <DatePicker onChange={handleChange} />
+    <Button type="primary" style={{ marginLeft: 8 }}>
+      Primary Button
+    </Button>
+    <div style={{ marginTop: 20 }}>
+      {/* Selected Date: {date ? date.format('YYYY-MM-DD') : 'None'} */}
+      <Alert message={`Selected Date: ${date ? date.format('YYYY-MM-DD') : 'None'}`} type="success" />
     </div>
+    
+  </div>
   );
 }
 
